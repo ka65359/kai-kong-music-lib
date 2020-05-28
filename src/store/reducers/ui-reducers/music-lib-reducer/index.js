5b71e4ee6129b1f29a6b6f2d5bcd99b037d6dfff
Kai <tehhavok@gmail.com>
2020-05-28 13:55:49 -0400

    update reducer

--- src/store/reducers/ui-reducers/music-lib-reducer/index.js
+++ src/store/reducers/ui-reducers/music-lib-reducer/index.js
@@ -1,25 +1,77 @@
 import { createReducer } from "redux-act";
-import { setSongs, clearSongs } from "store/actions";
+import {
+  setDataFetching,
+  setAllSongs,
+  clearAllSongs,
+  addSongToLibrary,
+  removeSongFromLibrary,
+  setPlaylists,
+  clearPlaylists
+  /*addPlaylist,
+  removePlaylist,
+  addSongToPlaylist,
+  removeSongFromPlaylist*/
+} from "store/actions";
 
 export const initialState = {
-  songs: []
+  dataFetching: false,
+  songs: [],
+  playlists: []
 };
 
 export default createReducer(
   {
-    [clearSongs]: (state) => {
+    [setDataFetching]: (state, payload) => {
+      let rslt = Object.assign({}, state, {
+        dataFetching: payload
+      });
+      return rslt;
+    },
+    [clearAllSongs]: (state) => {
       let rslt = Object.assign({}, state, {
         songs: initialState.songs
       });
       return rslt;
     },
-    [setSongs]: (state, payload) => {
+    [setAllSongs]: (state, payload) => {
       if (!payload) {
         console.error("New value is empty, reverting to intial");
         payload = initialState.songs;
       }
       let rslt = Object.assign({}, state, {
         songs: payload
+      });
+      return rslt;
+    },
+    [removeSongFromLibrary]: (state, payload) => {
+      let rslt = Object.assign({}, state, {
+        songs: payload
+      });
+      return rslt;
+    },
+    [addSongToLibrary]: (state, payload) => {
+      if (!payload) {
+        console.error("New value is empty, reverting to intial");
+        payload = initialState.songs;
+      }
+      let rslt = Object.assign({}, state, {
+        songs: payload
+      });
+      return rslt;
+    },
+    [clearPlaylists]: (state) => {
+      let rslt = Object.assign({}, state, {
+        playlists: initialState.playlists
+      });
+      return rslt;
+    },
+    [setPlaylists]: (state, payload) => {
+      if (!payload) {
+        console.error("New value is empty, reverting to intial");
+        payload = initialState.playlists;
+      }
+      let rslt = Object.assign({}, state, {
+        playlists: payload
       });
       return rslt;
     }
